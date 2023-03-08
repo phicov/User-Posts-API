7d4bb9dcdb88dfe5d48428ad69e1fe90a3567824
Philippe Covington <philippehcbusi@gmail.com>
2023-03-08 12:18:41 -0800

    API Called/ Successfully Planted On Frontend

---
+++ index.js
@@ -0,0 +1,24 @@
+// API 1: "https://jsonplaceholder.typicode.com/users"
+// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+const userListEl = document.querySelector('.user-list')
+
+async function main(){
+  const users = await fetch('https://jsonplaceholder.typicode.com/users')
+  const userData = await users.json()
+  userListEl.innerHTML = userData.map(
+    (user) => `
+    <div class="user">
+    <div class="user-card">
+      <div class="user-card__container">
+        <h3>${user.name}</h4>
+          <p><b>Email:</b> ${user.email}</p>
+          <p><b>Phone:</b> ${user.phone}</p>
+          <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+      </div>
+    </div>
+  </div>`
+  ).join('')
+}
+
+main()
