12ce8e84c646c81c9ae94def114bccb158605ee0
Philippe Covington <philippehcbusi@gmail.com>
2023-03-08 12:30:52 -0800

    User HTML Func, UserId local storage.

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ const userListEl = document.querySelector('.user-list')
 async function main(){
   const users = await fetch('https://jsonplaceholder.typicode.com/users')
   const userData = await users.json()
-  userListEl.innerHTML = userData.map(
-    (user) => `
-    <div class="user">
-    <div class="user-card">
-      <div class="user-card__container">
-        <h3>${user.name}</h4>
-          <p><b>Email:</b> ${user.email}</p>
-          <p><b>Phone:</b> ${user.phone}</p>
-          <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
-      </div>
-    </div>
-  </div>`
-  ).join('')
+  userListEl.innerHTML = userData.map(user => userHTML(user)).join('')
+}
+main()
+
+function showUserPosts(id){
+  localStorage.setItem("id", id)
+  window.location.href = `${window.location.origin}/user.html`
 }
 
-main()
+function userHTML(user){
+  return `
+    <div class="user" onclick="showUserPosts()">
+      <div class="user-card">
+        <div class="user-card__container">
+          <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+      </div>
+    </div>`
+}
